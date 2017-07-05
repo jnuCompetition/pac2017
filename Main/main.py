f63c2e00453b7b607bcb0272643f1a04d9892de0
zhpmatrix <zhpmatrix@gmail.com>
2017-07-05 21:04:57 +0800

    Fixed: train and predict

--- Main/main.py
+++ Main/main.py
@@ -3,11 +3,11 @@
 ##################################
 #   desc: data.py  
 #   author: zhpmatrix
-#   date:   2017-07-04
+#   date:   2017-07-05
 
 ###################################
 
-FLAG = 0 # Train: 0, Predict: 1
+FLAG = 1 # Train: 0, Predict: 1
 
 import numpy as np
 import pandas as pd
@@ -36,9 +36,22 @@ def train(x,y,params):
                   metrics=['accuracy'])
     
     train_num = int( (1-1.0/params['kfolds'])*x.shape[0] )
-    model.fit(x[:train_num], y[:train_num], batch_size = params['batch_size'], epochs=params['epochs'])
-    model.evaluate(x[train_num:], y[train_num:], batch_size =params['batch_size'])
+    model.fit(x[:train_num], y[:train_num], batch_size = params['batch_size'], epochs=params['epochs'],verbose=True)
+   
+    # Get loss and acc from test set
+    results = []
+    _results = {}
+    results = model.evaluate(x[train_num:], y[train_num:], batch_size =params['batch_size'],verbose=True)
+    _results['loss'] = results[0]
+    _results['acc'] = results[1]
+    print _results
+
     model.save(params['save'])
+
+def showCutWords(cutwords):
+     print '----------------'
+     for word in cutwords:
+         print word.encode('utf-8')
 
 def predict(model,sset,maxlen,wdict,wset,stopwords_path):
     '''
@@ -54,9 +67,10 @@ def predict(model,sset,maxlen,wdict,wset,stopwords_path):
     for s in sset:
         # Remove stop words
         cutwords = filterCmt( list(jieba.cut(s.replace('\n',''))),stopwords )
+        showCutWords(cutwords)
         s = np.array(word2vec(cutwords,wdict,wset,maxlen))
         s = s.reshape((1, s.shape[0]))
-        print model.predict_classes(s, verbose=0)[0]
+        print model.predict_classes(s, verbose=False)[0]
 
 if __name__ == '__main__':
     
@@ -72,7 +86,7 @@ if __name__ == '__main__':
     stopwords_path = 'stopwords'
 
     # Test set
-    sset = ['产品怎么这么差',
+    sset = ['一个严肃的问题，招行双币一卡通，VISA+银联，是否可以用卡号+有效期通过visa消费 借记卡也可以这么消费啊？太不安全了',
             '买东西很便宜',
             '一定要给别人介绍',
             'ApplePay这个东西很好呀']
@@ -90,7 +104,7 @@ if __name__ == '__main__':
         params['dropout'] = 0.5
         params['batch_size'] = 128
         params['kfolds'] = 5
-        params['epochs']=30
+        params['epochs']=2
         params['save'] = model_path
         
         train(x,y,params)    
